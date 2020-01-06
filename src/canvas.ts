78709f80e45be38f1f867d3772e6e56a1c367384
mitja <cmitja@gmail.com>
2020-01-06 18:27:37 +0100

    Initial commit.

---
+++ src/canvas.ts
@@ -0,0 +1,10 @@
+
+
+
+// *******************************************************
+
+export const renderGame = () => {};
+export const renderGameOverLite = () => (document.body.innerHTML += '<br/>GAME OVER!');
+export const noop = () => {};
+
+// *******************************************************
---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { animationFrame } from 'rxjs/scheduler/animationFrame';
+
+import { interval } from 'rxjs/observable/interval';
+import { fromEvent } from 'rxjs/observable/fromEvent';
+import { combineLatest } from 'rxjs/observable/combineLatest';
+import { of } from 'rxjs/observable/of';
+import { generate } from 'rxjs/observable/generate';
+
+import {
+  map,
+  filter,
+  scan,
+  startWith,
+  distinctUntilChanged,
+  share,
+  withLatestFrom,
+  tap,
+  skip,
+  takeWhile,
+  take,
+  switchMap,
+  first,
+  pluck,
+  mergeMap,
+  toArray
+} from 'rxjs/operators';
+
